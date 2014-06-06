749040d4fe4f759ed36cc1a087722b0c974edce7
Hiswe <hiswehalya@gmail.com>
2014-06-06 23:53:12 +0700

    [GULP] refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
 'use strict';
 
+var es          = require('event-stream');
 var gulp        = require('gulp');
 var wait        = require('gulp-wait');
+var clone       = require('gulp-clone');
 var rename      = require('gulp-rename');
 var iconfont    = require('gulp-iconfont');
-var clone       = require('gulp-clone');
-var es          = require('event-stream');
+var lazypipe    = require('lazypipe');
 var gulpFilter  = require('gulp-filter');
-var consolidate = require("gulp-consolidate");
+var consolidate = require('gulp-consolidate');
 // SERVER
-var open        = require('gulp-open');
 var lr          = require('tiny-lr')();
+var open        = require('gulp-open');
+var gulpLr      = require('gulp-livereload');
 var express     = require('express');
 var connectLr   = require('connect-livereload');
-var gulpLr      = require('gulp-livereload');
 
+/////////
+// UTILITIES
+/////////
 
 function getHexCode(text) {
   var code = text.charCodeAt(0);
@@ -26,6 +30,8 @@ function getHexCode(text) {
 function prependUnicode(path) {
   // get the letter
   var matches = /^.*_(.)$/.exec(path.basename)
+  // You can specify which unicode you're aiming in the name
+  // https://github.com/nfroidure/gulp-svgicons2svgfont/blob/master/src/index.js#L61
   // prepend the unicode
   var name = 'u' + getHexCode(matches[1]) + '-' + matches[1];
   path.basename = name;
@@ -36,39 +42,45 @@ function isLetter(file) {
 }
 
 /////////
+// LAZYPIPES
+/////////
+
+var uppercaseStream = lazypipe()
+  .pipe(clone)
+  .pipe(gulpFilter, isLetter)
+  .pipe(rename, function(path){
+    path.basename = path.basename.toUpperCase();
+  });
+
+/////////
 // BUILD
 /////////
 
+function onCodePoints(dst, options) {
+  // Generate css file
+  gulp.src('src/template.css')
+    .pipe(consolidate('lodash', {fontName: options.fontName, fontPath: './'}))
+    .pipe(rename(function nameCssFile(path) {
+      path.basename = options.fontName + '-font';
+    }))
+    .pipe(gulp.dest(dst))
+}
+
 gulp.task('build', function(){
   var src = gulp.src('src/dark/*.svg')
-
   // Generate the uppercase files
   var maj = src
-    .pipe(clone())
-    .pipe(gulpFilter(isLetter))
-    .pipe(rename(function(path){
-      path.basename = path.basename.toUpperCase()
-    }));
+    .pipe(uppercaseStream())
 
   // Generate the font
   es.merge(src, maj)
     .pipe(rename(prependUnicode))
-    .pipe(iconfont({
-      fontName: 'hiso-dark', // required
-      appendCodepoints: false // recommended option to true
-    }).on('codepoints', function(codepoints, options) {
-        // Generate css file
-        gulp.src('src/template.css')
-          .pipe(consolidate('lodash', {
-              fontName: options.fontName,
-              fontPath: './'
-          }))
-          .pipe(rename(function(path){path.basename = options.fontName + '-font'}))
-          .pipe(gulp.dest('dst/dark'))
-
-        // CSS templating, e.g.
-        console.log(options);
-      }))
+    .pipe(
+      iconfont({
+        fontName: 'hiso-dark', // required
+        appendCodepoints: false // recommended option to true
+      })
+      .on('codepoints', function(codepoints, options) { onCodePoints('dst/dark', options) }))
     .pipe(gulp.dest('dst/dark'))
 });
 
