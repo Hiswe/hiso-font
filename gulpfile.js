22c0f838069c733971a277a8c5baba90b1fb0d1a
Hiswe <hiswehalya@gmail.com>
2014-06-07 18:31:00 +0700

    [FONT] add plain and bright variant

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ var es          = require('event-stream');
 var gulp        = require('gulp');
 var wait        = require('gulp-wait');
 var clone       = require('gulp-clone');
+var unorm       = require('unorm');
 var rename      = require('gulp-rename');
+var svgmin      = require('gulp-svgmin');
 var iconfont    = require('gulp-iconfont');
 var lazypipe    = require('lazypipe');
 var gulpFilter  = require('gulp-filter');
-var unorm       = require('unorm');
 // See this for categories
 // http://unicode.org/Public/UNIDATA/UnicodeData.txt
 var unicodeList = _.merge(
@@ -38,7 +39,6 @@ var connectLr   = require('connect-livereload');
 // https://github.com/nfroidure/gulp-svgicons2svgfont/blob/master/src/index.js#L61
 function cleanName(path) {
   var name = unorm.nfc(path.basename);
-  // console.log(name);
   if (/^.*_(.)$/.test(name)) {
     path.basename = prependUnicode(name);
   }
@@ -65,11 +65,11 @@ function cleanUnicode(name) {
 
 function privateUse(name) {
   var matches = /^.*_p\d+-(.+)$/.exec(name);
-  return 'HISWE ' + matches[1].toUpperCase();
+  return 'HISO ' + matches[1].toUpperCase();
 }
 
 function getHexCode(text) {
-  var code = text.charCodeAt(0);
+  var code = (typeof text === 'string') ? text.charCodeAt(0) : text;
   var codeHex = code.toString(16).toUpperCase();
   while (codeHex.length < 4) { codeHex = '0' + codeHex; }
   return codeHex
@@ -103,6 +103,34 @@ gulp.task('unicode', function (cb) {
   cb();
 })
 
+function byCodepoint(a,b){
+  if (a.codepoint < b.codepoint) return -1;
+  if (a.codepoint > b.codepoint) return 1;
+  return 0;
+}
+
+function formatCodepoints(codepoints) {
+  var digit = [];
+  var letter = [];
+  var priv = [];
+  var other = [];
+
+  codepoints.forEach(function(codepoint) {
+    codepoint.letter  = String.fromCharCode(codepoint.codepoint);
+    codepoint.hex     = getHexCode(codepoint.codepoint);
+    if (/LATIN/.test(codepoint.name)) return letter.push(codepoint);
+    if (/DIGIT/.test(codepoint.name)) return digit.push(codepoint);
+    if (/HISO/.test(codepoint.name))  return priv.push(codepoint);
+    other.push(codepoint);
+  });
+  return {
+    digit:  digit.sort(byCodepoint),
+    letter: letter.sort(byCodepoint),
+    priv:   priv.sort(byCodepoint),
+    other:  other.sort(byCodepoint)
+  }
+}
+
 /////////
 // LAZYPIPES
 /////////
@@ -118,31 +146,56 @@ var uppercaseStream = lazypipe()
 // BUILD
 /////////
 
-function onCodePoints(dst, options) {
-  // Generate css file
+function onCodePoints(codepoints, options) {
+  // Generate html demo file
+  gulp.src('src/index.html')
+    .pipe(consolidate('lodash', {codepoints: formatCodepoints(codepoints)}))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./'))
+
+  // Generate css font file
   gulp.src('src/template.css')
     .pipe(consolidate('lodash', {fontName: options.fontName, fontPath: './'}))
     .pipe(rename(function nameCssFile(path) {
       path.basename = options.fontName + '-font';
     }))
-    .pipe(gulp.dest(dst))
+    .pipe(gulp.dest('dst'))
 }
 
-gulp.task('build', function(){
-  var src = gulp.src('src/dark/*.svg')
-  // Generate the uppercase files
-  var maj = src.pipe(uppercaseStream())
-  // Generate the font
-  es.merge(src, maj)
-    .pipe(rename(cleanName))
-    .pipe(
-      iconfont({
-        fontName: 'hiso-dark'// required
-        // , descent: 5000
-      })
-      .on('codepoints', function(codepoints, options) { onCodePoints('dst/dark', options) }))
-    .pipe(gulp.dest('dst/dark'))
+gulp.task('min', function(){
+  return gulp.src('src/**/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest('src'))
 });
+
+// Generate font files
+
+gulp.task('dark', ['min'], function(){
+  var src = gulp.src('src/dark/*.svg')
+  var maj = src.pipe(uppercaseStream())
+  es.merge(src, maj).pipe(rename(cleanName))
+    .pipe(iconfont({ fontName: 'hiso-dark' }).on('codepoints', onCodePoints))
+    .pipe(gulp.dest('dst'))
+});
+
+
+gulp.task('plain', ['min'], function(){
+  var src = gulp.src('src/plain/*.svg')
+  var maj = src.pipe(uppercaseStream())
+  es.merge(src, maj).pipe(rename(cleanName))
+    .pipe(iconfont({ fontName: 'hiso-plain' }).on('codepoints', onCodePoints))
+    .pipe(gulp.dest('dst'))
+});
+
+gulp.task('bright', ['min'], function(){
+  var src = gulp.src('src/bright/*.svg')
+  var maj = src.pipe(uppercaseStream())
+  es.merge(src, maj).pipe(rename(cleanName))
+    .pipe(iconfont({ fontName: 'hiso-bright' }).on('codepoints', onCodePoints))
+    .pipe(gulp.dest('dst'))
+});
+
+gulp.task('build', ['dark', 'plain', 'bright']);
 
 /////////
 // SERVER
